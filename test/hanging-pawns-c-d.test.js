113b18aa6e71ec60eb7760b9bc1ceb55377e04a9
Kevin Ludwig <kevin.ludwig@gmail.com>
2026-02-16 15:20:43 -0800

    iqp and hanging pawns tests

--- test/hanging-pawns-c-d.test.js
+++ test/hanging-pawns-c-d.test.js
@@ -1,8 +1,27 @@
 'use strict';
-const { describe } = require('node:test');
+const { describe, it } = require('node:test');
 const { detectStructures, getStructure } = require('../src/index.ts');
-const { BitboardChess } = require('bitboard-chess');
+const bitboardChess = require('bitboard-chess');
+const { playSequenceAndAssertStructure } = require('./helpers.js');
+
+const Chess = bitboardChess.default ?? bitboardChess;
 
 describe('Hanging Pawns (c-d)', () => {
-  // N-tests: play move sequence, get position, assert detectStructures(position) includes this structure
+  it('reaches Hanging Pawns (c-d) after 1. e4 c5 2. c3 Nf6 3. e5 Nd5 4. d4 cxd4 5. cxd4 e6 ... Qe2 Bb7', () => {
+    const moves = [
+      'e4', 'c5', 'c3', 'Nf6', 'e5', 'Nd5', 'd4', 'cxd4', 'cxd4', 'e6',
+      'Nf3', 'd6', 'exd6', 'Bxd6', 'Nc3', 'Nxc3', 'bxc3', 'O-O', 'Bd3', 'a6',
+      'O-O', 'b5', 'Qe2', 'Bb7',
+    ];
+    playSequenceAndAssertStructure(Chess, moves, detectStructures, 'Hanging Pawns (c-d)');
+  });
+
+  it('reaches Hanging Pawns (c-d) after 1. d4 Nf6 2. c4 e6 ... dxc5 bxc5 14. Nb3', () => {
+    const moves = [
+      'd4', 'Nf6', 'c4', 'e6', 'g3', 'Bb4+', 'Bd2', 'Bxd2+', 'Nxd2', 'd5',
+      'Bg2', 'O-O', 'Ng1f3', 'b6', 'O-O', 'Bb7', 'Rc1', 'Nbd7', 'cxd5', 'exd5',
+      'Ne5', 'c5', 'Nxd7', 'Nxd7', 'dxc5', 'bxc5', 'Nb3',
+    ];
+    playSequenceAndAssertStructure(Chess, moves, detectStructures, 'Hanging Pawns (c-d)');
+  });
 });
--- test/iqp.test.js
+++ test/iqp.test.js
@@ -1,8 +1,25 @@
 'use strict';
-const { describe } = require('node:test');
+const { describe, it } = require('node:test');
 const { detectStructures, getStructure } = require('../src/index.ts');
-const { BitboardChess } = require('bitboard-chess');
+const bitboardChess = require('bitboard-chess');
+const { playSequenceAndAssertStructure } = require('./helpers.js');
+
+const Chess = bitboardChess.default ?? bitboardChess;
 
 describe('IQP', () => {
-  // N-tests: play move sequence, get position, assert detectStructures(position) includes this structure
+  it('reaches IQP after QGA line (c4 dxc4 ... exd4 b5 a4 b4 Nbd2)', () => {
+    const moves = [
+      'd4', 'd5', 'c4', 'dxc4', 'Nf3', 'Nf6', 'e3', 'e6', 'Bxc4', 'Be7',
+      'O-O', 'a6', 'Bb3', 'c5', 'Qe2', 'cxd4', 'exd4', 'b5', 'a4', 'b4', 'Nbd2',
+    ];
+    playSequenceAndAssertStructure(Chess, moves, detectStructures, 'IQP');
+  });
+
+  it('reaches IQP after 1. d4 d5 2. c4 e6 3. Nc3 c5 ... cxd4 Nxd4 Bg5', () => {
+    const moves = [
+      'd4', 'd5', 'c4', 'e6', 'Nc3', 'c5', 'cxd5', 'exd5', 'Nf3', 'Nc6',
+      'g3', 'cxd4', 'Nxd4', 'Be7', 'O-O', 'O-O', 'Bg5',
+    ];
+    playSequenceAndAssertStructure(Chess, moves, detectStructures, 'IQP');
+  });
 });
