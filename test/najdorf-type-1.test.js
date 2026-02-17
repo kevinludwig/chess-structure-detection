b4daa6eb145287b2a0ac09cbb8442335394a73da
Kevin Ludwig <kevin.ludwig@gmail.com>
2026-02-16 23:46:11 -0800

    najdorf type 1 tests

--- test/najdorf-type-1.test.js
+++ test/najdorf-type-1.test.js
@@ -1,8 +1,26 @@
 'use strict';
-const { describe } = require('node:test');
+const { describe, it } = require('node:test');
 const { detectStructures, getStructure } = require('../src/index.ts');
-const { BitboardChess } = require('bitboard-chess');
+const bitboardChess = require('bitboard-chess');
+const { playSequenceAndAssertStructure } = require('./helpers.js');
+
+const Chess = bitboardChess.default ?? bitboardChess;
 
 describe('Najdorf Type 1', () => {
-  // N-tests: play move sequence, get position, assert detectStructures(position) includes this structure
+  it('Sicilian Najdorf (Be3) reaches Najdorf Type 1 after 1. e4 c5 2. Nf3 d6 3. d4 cxd4 4. Nxd4 Nf6 5. Nc3 a6 6. Be3 e5 7. Nb3 Be6 8. f3 b5 9. Qd2 Nbd7 10. Nd5 Bxd5 11. exd5', () => {
+    const moves = [
+      'e4', 'c5', 'Nf3', 'd6', 'd4', 'cxd4', 'Nxd4', 'Nf6', 'Nc3', 'a6',
+      'Be3', 'e5', 'Nb3', 'Be6', 'f3', 'b5', 'Qd2', 'Nbd7', 'Nd5', 'Bxd5', 'exd5',
+    ];
+    playSequenceAndAssertStructure(Chess, moves, detectStructures, 'Najdorf Type 1');
+  });
+
+  it('Sicilian Najdorf (h3 g4) reaches Najdorf Type 1 after 1. e4 c5 2. Nf3 d6 3. d4 cxd4 4. Nxd4 Nf6 5. Nc3 a6 6. h3 e5 7. Nde2 h5 8. g4 Be6 9. Bg2 Nbd7 10. Be3 b5 11. Nd5 Bxd5 12. exd5', () => {
+    const moves = [
+      'e4', 'c5', 'Nf3', 'd6', 'd4', 'cxd4', 'Nxd4', 'Nf6', 'Nc3', 'a6',
+      'h3', 'e5', 'Nde2', 'h5', 'g4', 'Be6', 'Bg2', 'Nbd7', 'Be3', 'b5',
+      'Nd5', 'Bxd5', 'exd5',
+    ];
+    playSequenceAndAssertStructure(Chess, moves, detectStructures, 'Najdorf Type 1');
+  });
 });
