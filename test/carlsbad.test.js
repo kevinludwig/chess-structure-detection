2a0847760b312ec1631aa3692f5a87f7511d5df2
Kevin Ludwig <kevin.ludwig@gmail.com>
2026-02-16 16:30:31 -0800

    carlsbad and grunfeld exchange center

--- test/carlsbad.test.js
+++ test/carlsbad.test.js
@@ -1,8 +1,25 @@
 'use strict';
-const { describe } = require('node:test');
+const { describe, it } = require('node:test');
 const { detectStructures, getStructure } = require('../src/index.ts');
-const { BitboardChess } = require('bitboard-chess');
+const bitboardChess = require('bitboard-chess');
+const { playSequenceAndAssertStructure } = require('./helpers.js');
+
+const Chess = bitboardChess.default ?? bitboardChess;
 
 describe('Carlsbad', () => {
-  // N-tests: play move sequence, get position, assert detectStructures(position) includes this structure
+  it('Queen\'s Gambit Declined Exchange reaches Carlsbad after 1. d4 d5 2. c4 e6 3. Nc3 Nf6 4. cxd5 exd5 5. Bg5 Be7 6. e3 c6', () => {
+    const moves = [
+      'd4', 'd5', 'c4', 'e6', 'Nc3', 'Nf6', 'cxd5', 'exd5', 'Bg5', 'Be7',
+      'e3', 'c6',
+    ];
+    playSequenceAndAssertStructure(Chess, moves, detectStructures, 'Carlsbad');
+  });
+
+  it('Caro-Kann Exchange reaches Carlsbad after 1. e4 c6 2. d4 d5 3. exd5 cxd5 4. Bd3 Nf6 5. c3 Nc6 6. Bf4 e6 7. Nf3', () => {
+    const moves = [
+      'e4', 'c6', 'd4', 'd5', 'exd5', 'cxd5', 'Bd3', 'Nf6', 'c3', 'Nc6',
+      'Bf4', 'e6', 'Nf3',
+    ];
+    playSequenceAndAssertStructure(Chess, moves, detectStructures, 'Carlsbad');
+  });
 });
--- test/grunfeld-exchange-center.test.js
+++ test/grunfeld-exchange-center.test.js
@@ -1,8 +1,17 @@
 'use strict';
-const { describe } = require('node:test');
+const { describe, it } = require('node:test');
 const { detectStructures, getStructure } = require('../src/index.ts');
-const { BitboardChess } = require('bitboard-chess');
+const bitboardChess = require('bitboard-chess');
+const { playSequenceAndAssertStructure } = require('./helpers.js');
+
+const Chess = bitboardChess.default ?? bitboardChess;
 
 describe('Grunfeld Exchange Center', () => {
-  // N-tests: play move sequence, get position, assert detectStructures(position) includes this structure
+  it('Grunfeld Defense Exchange reaches Grunfeld Exchange Center after 1. d4 Nf6 2. c4 g6 3. Nc3 d5 4. Nf3 Bg7 5. cxd5 Nxd5 6. e4 Nxc3 7. bxc3 c5 8. Rb1 O-O', () => {
+    const moves = [
+      'd4', 'Nf6', 'c4', 'g6', 'Nc3', 'd5', 'Nf3', 'Bg7', 'cxd5', 'Nxd5',
+      'e4', 'Nxc3', 'bxc3', 'c5', 'Rb1', 'O-O',
+    ];
+    playSequenceAndAssertStructure(Chess, moves, detectStructures, 'Grunfeld Exchange Center');
+  });
 });
